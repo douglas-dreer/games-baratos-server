acc8cd27f3c5e0716300b5d404263e2eeed31685
Douglas Dreer <douglas.dreer.ext@dasa.com.br>
2023-12-04 14:43:47 -0300

    Melhorias no controller

--- src/main/kotlin/br/com/gamebarato/controller/nintendo/eshop/NintendoEshopController.kt
+++ src/main/kotlin/br/com/gamebarato/controller/nintendo/eshop/NintendoEshopController.kt
@@ -17,15 +17,25 @@ class NintendoEshopController @Autowired constructor(var service: NintendoEshopS
 
     @GetMapping("/jogo")
     fun listarTodosJogosPorRegiao(
-        @RequestParam(name = "page", defaultValue = "0") page: String,
-        @RequestParam(name = "pageSize", defaultValue = "100") pageSize: String
+        @RequestParam(name = "page", defaultValue = "0") page: Int,
+        @RequestParam(name = "pageSize", defaultValue = "100") pageSize: Int,
+        @RequestParam(name = "regiao", defaultValue = "BR") regiao: String
     ): ResponseEntity<GameResponse> {
-        return ResponseEntity.ok(this.service.listar())
+        return ResponseEntity.ok(this.service.listar(page, pageSize, regiao))
     }
 
     @GetMapping("/jogo/{nuuid}")
     fun buscarJogoPorNuuid(@PathVariable("nuuid") nuuid: String): ResponseEntity<GameResponse> {
         val resultado = this.service.buscarPorUUID(nuuid)
+        return ResponseEntity.ok(resultado)
+    }
+
+    @GetMapping("/jogo/promocao")
+    fun buscarJogoEmPromocao(
+        @RequestParam(name = "page", defaultValue = "0") page: Int,
+        @RequestParam(name = "pageSize", defaultValue = "100") pageSize: Int
+    ): ResponseEntity<GameResponse> {
+        val resultado = this.service.buscarJogoEmPromocao(page, pageSize)
         return ResponseEntity.ok(resultado)
     }
 
