e66dbf91e26b4384b3818fbb35e9328bbe1bc818
Douglas Dreer <douglas.dreer.ext@dasa.com.br>
2023-12-04 14:42:51 -0300

    Correção e adaptação nos modelos

--- src/main/kotlin/br/com/gamebarato/model/nintendo/Title.kt
+++ src/main/kotlin/br/com/gamebarato/model/nintendo/Title.kt
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 
 
-data class Title @JsonCreator constructor (
-    @JsonProperty("value") var value        : String?           = null,
-    @JsonProperty("matchLevel") var matchLevel   : String?           = null,
-    @JsonProperty("matchedWords") var matchedWords : ArrayList<String> = arrayListOf()
+data class Title @JsonCreator constructor(
+    @JsonProperty("value") var value: String? = null,
+    @JsonProperty("matchLevel") var matchLevel: String? = null,
+    @JsonProperty("matchedWords") var matchedWords: ArrayList<String> = arrayListOf()
 
 )
